26dad56a3ae5ab1fb9d7aca70169fe54f70b13e1
junjy007 <junjy007@gmail.com>
2021-02-27 02:29:29 +0300

    Compile with OpenCV 4

--- include/FrameDrawer.h
+++ include/FrameDrawer.h
@@ -26,6 +26,7 @@
 
 #include<opencv2/core/core.hpp>
 #include<opencv2/features2d/features2d.hpp>
+#include<opencv2/core/core_c.h>
 
 #include<mutex>
 #include <unordered_set>
--- include/Sim3Solver.h
+++ include/Sim3Solver.h
@@ -21,6 +21,7 @@
 #define SIM3SOLVER_H
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/core_c.h>
 #include <vector>
 
 #include "KeyFrame.h"
